0288d5fd4f4be63d7ed69ba0fe726d9edfdd0ee3
qun.wang <qun.wang@reorientgroup.com>
2016-11-30 18:48:45 +0800

    modify hexo start param

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,11 +5,9 @@ echo "$@" | awk -F ' ' '{print $1}' | xargs git config --global user.name
 echo "$@" | awk -F ' ' '{print $2}' | xargs git config --global user.email
 
 if [ "$3" = 's' ] || [ "$3" = 'server' ]; then
-    set -- /usr/bin/hexo s -p 80
-fi
-
-if [ "$3" = 'd' ] || [ "$3" = 'deploy' ]; then
-    set -- /usr/bin/hexo cl && /usr/bin/hexo d -g
+    set -- /usr/bin/env hexo s -p 80
+elif [ "$3" = 'd' ] || [ "$3" = 'deploy' ]; then
+    set -- /usr/bin/env hexo cl && /usr/bin/env hexo d -g
 fi
 
 exec "$@"
