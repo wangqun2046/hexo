a4b637ab587a0405d56d4e91c2151a01d615d692
qun.wang <qun.wang@reorientgroup.com>
2016-12-05 17:20:36 +0800

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,29 @@ var uglify = require('gulp-uglify')
 var cleancss = require('gulp-clean-css')
 var htmlmin = require("gulp-htmlmin")
 var pump = require('pump')
-
 var dir = './public'
 
-gulp.task('default',function(cb) {
+gulp.task('minify-css', function(cb) {
+  pump([
+    gulp.src('./public/**/*.css'),
+    cleancss({compatibility: 'ie8'}),
+    gulp.dest(dir)
+  ])
+})
+
+gulp.task('minify-js', function(cb) {
+    pump([
+        gulp.src('./public/**/*.js'),
+        uglify(),
+        gulp.dest(dir)
+    ], cb)
+})
+
+gulp.task('minify-html', function(cb) {
   var opts = {
          removeComments: true,
          collapseWhitespace: true,
          removeEmptyAttributes: true,
-         removeEmptyElements: true,
          minifyJS: true,
          minifyCSS: true,
          minifyURLs: true,
@@ -23,11 +37,11 @@ gulp.task('default',function(cb) {
     gulp.src('./public/**/*.html'),
     inline({
         base: './public/',
-        js: uglify,
-        css: cleancss,
         disabledTypes: ['svg', 'img']
     }),
     htmlmin(opts),
     gulp.dest(dir)
   ], cb)
 })
+
+gulp.task('default', ['minify-css', 'minify-js', 'minify-html'])
